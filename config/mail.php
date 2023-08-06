c87551f47b04e3d384c706606fd1226b1bd9d36a
Nhut Linh <trannhutlinh0925@gmail.com>
2023-08-06 12:58:49 +0700

    gửi mail đơn giản

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' => 'smtp.gmail.com',
+            'port' => 587,
+            'encryption' => 'tls',
+            'username' => 'trannhutlinh0925@gmail.com',
+            'password' => 'ggrxmxkjxsbjvylj',
             'timeout' => null,
         ],
 
@@ -91,9 +91,14 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => 'trannhutlinh0925@gmail.com',
+        'name' => 'Cảm ơn đã mua hàng',
     ],
+
+//    'from' => [
+//        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+//        'name' => env('MAIL_FROM_NAME', 'Example'),
+//    ],
 
     /*
     |--------------------------------------------------------------------------
